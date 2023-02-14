f97d5e6929737121312af6264888f35d63ee6c9f
zhangbofei <zhangbofei@163.com>
2023-02-14 16:45:23 +0800

    ✨新增(Vue): 更新首页，添加markdown功能

---
+++ src/components/Markdown/Markdown.vue
@@ -0,0 +1,20 @@
+<template>
+  <textarea v-model="textValue" cols="30" rows="10"></textarea>
+  <div v-html="mdxValue"></div>
+</template>
+
+<script setup lang="ts">
+import { marked } from 'marked'
+const textValue = ref('# 这里是标题')
+const mdxValue = computed(() => marked(textValue.value))
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+<script lang="ts">
+export default {
+  name: 'HomePage'
+}
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
-    <textarea v-model="textValue" cols="30" rows="10"></textarea>
-    <div v-html="mdxValue"></div>
-  </div>
+  <h1>
+    您还没有配置 home 页面
+  </h1>
+  <h2>
+    您可以先跳转到
+    <router-link to="/components" class="t-pink">
+      Components页面
+    </router-link>
+  </h2>
 </template>
 
-<script setup lang="ts">
-import { marked } from 'marked'
-const textValue = ref('# 这里是标题')
-const mdxValue = computed(() => marked(textValue.value))
+<script lang="ts" setup>
 </script>
 
 <style scoped lang="scss">
-
+h1 {
+  font-size: 2rem;
+}
 </style>
-
 <script lang="ts">
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  inheritAttrs: false
 }
 </script>
